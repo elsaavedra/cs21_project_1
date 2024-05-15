01fd007c75d0538fd0de2853a11ba9b6e4a28ed3
blissful <72906589+BluNuggets@users.noreply.github.com>
2024-05-15 21:29:11 +0800

    feat: add "macros" files

---
+++ macros.c
@@ -0,0 +1,21 @@
+#include "macros.h"
+
+char *print_str(){ // int *pc, char *label
+    return "print str called\n";
+}
+
+char *read_str(){ // int *pc, char *label, int len
+    return "read str called\n";
+}
+
+char *print_integer(){ // int *pc, char *label
+    return "print int called\n";
+}
+
+char *read_integer(){
+    return "read int called\n";
+}
+
+char *exit_macro(){
+    return "exit code\n";
+}
---
+++ macros.h
@@ -0,0 +1,16 @@
+#ifndef _MACROS_H_
+#define _MACROS_H_
+
+char *print_str();
+
+char *print_str();
+
+char *read_str();
+
+char *print_integer();
+
+char *read_integer();
+
+char *exit_macro();
+
+#endif
